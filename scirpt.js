b24c052c20ea27139dd7e5ebbd91c3bdc8e0971f
Subash-coder56 <subash15461645@gmail.com>
2024-09-01 08:13:09 -0700

    ffdsfds

---
+++ scirpt.js
@@ -0,0 +1,28 @@
+let a = document.getElementById("img");
+
+function myfunon()
+{
+    a.style.animationDuration = 3 + "s";
+
+}
+function myfunoff()
+{
+    a.style.animationDuration = 0 + "s";
+    
+}
+function myfun1()
+{
+    a.style.animationDuration = 1 + "s";
+    
+}
+function myfun2()
+{
+
+    a.style.animationDuration = 0.5 + "s";
+    
+}
+function myfun3()
+{
+    a.style.animationDuration = 0.1 + "s";
+    
+}
